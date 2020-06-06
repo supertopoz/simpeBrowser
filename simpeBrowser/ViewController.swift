9ee4a562b922422a6e26004f22275498b83514ac
supertopoz <deepsleepsounds@gmail.com>
2020-06-06 13:30:06 +0900

    Simple Webnavigation done

--- simpeBrowser/ViewController.swift
+++ simpeBrowser/ViewController.swift
@@ -21,11 +21,32 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         // Do any additional setup after loading the view.
         let url = URL(string: "https://sendbird-sample-x.firebaseapp.com/?user=new_user_2")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
         
+
+    }
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction){
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
     
     
